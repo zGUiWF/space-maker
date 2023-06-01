d80517dedbd70ef134fa17c96427ba444451ebb4
Guilherme Winck Ferrari <guiferrari27@gmail.com>
2023-06-01 20:15:58 -0300

    Continuação do projeto: foi adicionado marcação das estrelas junto com nome e posição, as ligações entre as estrelas, além de alguns ajustes de boas praticas de programação

--- funções.py
+++ funções.py
@@ -1,8 +1,17 @@
+import pygame
 from tkinter import Tk, simpledialog
+
+#definir tamanho da tela
+tamanho = (1000, 550)
+tela = pygame.display.set_mode(tamanho)
 
 #pedir o nome da estrela
 def solicitarNomeEstrela(posicao):
-    Tk().withdraw()
-    nomeEstrela = simpledialog.askstring("Nome da Estrela", "Digite o nome da estrela:")
-    Tk().destroy()
-    return nomeEstrela, posicao
+    root = Tk()
+    root.withdraw()
+    nome_estrela = simpledialog.askstring("Nome da Estrela", "Digite o nome da estrela:")
+    root.destroy()
+    if nome_estrela is not None and nome_estrela.strip() != "":
+        return nome_estrela
+    else:
+        return "Desconhecido"
--- spaceMaker.py
+++ spaceMaker.py
@@ -6,12 +6,17 @@ pygame.init()
 
 #cores
 branco = (255,255,255)
-
-#obter tamanho da tela
-largura, altura = pygame.display.Info().current_w, pygame.display.Info().current_h
+preto = (0,0,0)
 
 #definir tamanho da tela
-tela = pygame.display.set_mode((largura, altura), pygame.FULLSCREEN)
+tamanho = (1000,550)
+tela = pygame.display.set_mode(tamanho)
+
+#marcações das estrelas
+marcacoes = []
+
+#linhas entre as estrelas
+linhas = []
 
 executando = True
 while executando:
@@ -20,9 +25,33 @@ while executando:
             executando = False
         elif evento.type == pygame.MOUSEBUTTONDOWN:
             if evento.button == 1:
-                #posição do mouse após o click
                 posicao = pygame.mouse.get_pos()
-                nomeEstrela, posicao = solicitarNomeEstrela(posicao)
+                nome_estrela = solicitarNomeEstrela(posicao)
+                if nome_estrela is not None:
+                    marcacao = {"nome": nome_estrela, "posicao": posicao}
+                    marcacoes.append(marcacao)
+
+                    #verifica se tiver apenas 1 marcação não marcar
+                    if len(marcacoes) >= 2:
+                        posicao1 = marcacoes[-2]['posicao']
+                        posicao2 = marcacoes[-1]['posicao']
+                        linha = (posicao1, posicao2)
+                        linhas.append(linha)
+    
+    tela.fill(preto)
+
+    #desenha linha entre as marcações
+    for linha in linhas:
+        pygame.draw.line(tela, (255, 255, 255), linha[0], linha[1], 2)
+
+    for marcacao in marcacoes:
+        nome = marcacao["nome"] #nome dado a estrela
+        posicao = marcacao["posicao"] #posição da estrela
+        pygame.draw.circle(tela, (255, 255, 255), posicao, 10) #desenha circulo marcador da estrela
+        texto = f"{nome} - Posição: {posicao}"
+        fonte = pygame.font.Font(None, 20)
+        textoRenderizado = fonte.render(texto, True, (255, 255, 255)) 
+        tela.blit(textoRenderizado, (posicao[0] + 15, posicao[1] - 10))
 
     pygame.display.update()
 
