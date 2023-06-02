f9edea13d96f21c108d22f9e6eb3c40cbb22aada
Guilherme Winck Ferrari <guiferrari27@gmail.com>
2023-06-02 17:49:33 -0300

    adição de teclas de entrada e efeito sonoro: foi adicionado as teclas 'z' para salvar as marcações, 'x' para exibir as marcações na tela e 'c' para excluir as marcações, e adicionado audio de som ambiente

--- funções.py
+++ funções.py
@@ -19,3 +19,27 @@ def calcularDistancia(posicao1, posicao2):
     x2, y2 = posicao2
     distancia = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return round(distancia, 2)
+
+# Função para salvar as marcações em um arquivo
+def salvarMarcacoes(arquivo, marcacoes):
+    with open(arquivo, "w") as f:
+        for marcacao in marcacoes:
+            nome = marcacao["nome"]
+            posicao = marcacao["posicao"]
+            linha = f"{nome},{posicao[0]},{posicao[1]}\n"
+            f.write(linha)
+
+def carregarMarcacoes(arquivo):
+    marcacoes = []
+    with open(arquivo, "r") as f:
+        for linha in f:
+            nome, x, y = linha.strip().split(",")
+            posicao = (int(x), int(y))
+            marcacao = {"nome": nome, "posicao": posicao}
+            marcacoes.append(marcacao)
+    return marcacoes
+
+def excluirMarcacoes(arquivo):
+    with open(arquivo, "w") as f:
+        pass
+    
--- spaceMaker.py
+++ spaceMaker.py
@@ -1,16 +1,28 @@
 import pygame
 from tkinter import Tk
-from funções import solicitarNomeEstrela, calcularDistancia
+from funções import solicitarNomeEstrela, calcularDistancia, salvarMarcacoes, carregarMarcacoes, excluirMarcacoes
+
+def escrverTexto(texto,cor,x,y,tamanhaFonte):
+    fonte = pygame.font.Font(None,tamanhaFonte)
+    textoRenderizado = fonte.render(texto, True, cor)
+    tela.blit(textoRenderizado, (x, y))
 
 pygame.init()
 
 #cores
 branco = (255,255,255)
-preto = (0,0,0)
+vermelho = (255,0,0)
+amarelo = (255,255,0)
 
 #definir tamanho da tela
 tamanho = (1000,550)
 tela = pygame.display.set_mode(tamanho)
+
+#fonte
+fonte = pygame.font.Font(None,20)
+
+#fps
+fps = 60
 
 #marcações das estrelas
 marcacoes = []
@@ -18,10 +30,27 @@ marcacoes = []
 #linhas entre as estrelas
 linhas = []
 
-pygame.display.set_caption("Space Marker")
-fundo = pygame.image.load("bg.jpg")
-icone = pygame.image.load("space.png")
+#carregamento de arquivos
+pygame.display.set_caption('Space Marker')
+fundo = pygame.image.load('bg.jpg')
+icone = pygame.image.load('space.png')
 pygame.display.set_icon(icone)
+
+#pasta das marcações
+arquivoMarcacoes = 'marcacoes.txt'
+
+#verifica se á arquivos na pasta 
+if pygame.key.get_pressed()[pygame.K_x]:
+    if arquivoMarcacoes:
+        marcacoes = carregarMarcacoes(arquivoMarcacoes)
+    else:
+        marcacoes = []
+else:
+    marcacoes = []
+
+#som ambiente
+pygame.mixer.music.load("SpaceAudio.mp3")
+pygame.mixer.music.play(-1)
 
 executando = True
 while executando:
@@ -33,7 +62,7 @@ while executando:
                 posicao = pygame.mouse.get_pos()
                 nomeEstrela = solicitarNomeEstrela(posicao)
                 if nomeEstrela is not None:
-                    marcacao = {"nome": nomeEstrela, "posicao": posicao} #dicionario com nome e posção da estrela
+                    marcacao = {'nome': nomeEstrela, 'posicao': posicao} #dicionario com nome e posção da estrela
                     marcacoes.append(marcacao)
 
                     #verifica se tiver apenas 1 marcação não marcar
@@ -42,32 +71,56 @@ while executando:
                         posicao2 = marcacoes[-1]['posicao']
                         linha = (posicao1, posicao2)
                         linhas.append(linha)
-    
+
+        #teclas para interação com as marcações               
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_z:
+                if arquivoMarcacoes:
+                    salvarMarcacoes(arquivoMarcacoes, marcacoes)
+            elif evento.key == pygame.K_x:
+                if arquivoMarcacoes:
+                    marcacoes = carregarMarcacoes(arquivoMarcacoes)
+                    linhas = []
+                    for i in range(len(marcacoes) - 1):
+                        posicao1 = marcacoes[i]['posicao']
+                        posicao2 = marcacoes[i + 1]['posicao']
+                        linha = (posicao1, posicao2)
+                        linhas.append(linha)
+            elif evento.key == pygame.K_c:
+                if arquivoMarcacoes:
+                    excluirMarcacoes(arquivoMarcacoes)
+                    marcacoes = []
+                    linhas = []
+
+    #fundo e indicadores de comandos
     tela.blit(fundo,(0,0))
+    escrverTexto('Tecle (z) para salvar as marcações',branco,5,10,20)
+    escrverTexto('Tecle (x) para carregar as marcações',branco,5,30,20)
+    escrverTexto('Tecle (z) para excluir as marcações',branco,5,50,20)
 
     #desenha linha entre as marcações
     for linha in linhas:
         posicao1, posicao2 = linha
-        pygame.draw.line(tela, (255, 255, 255), posicao1, posicao2, 2)
+        pygame.draw.line(tela, branco, posicao1, posicao2, 2)
 
         #escreve a distancia entre as marcações
         meioXdaLinha = (posicao1[0] + posicao2[0]) // 2
         meioYdaLinha = (posicao1[1] + posicao2[1]) // 2
         distancia = calcularDistancia(posicao1, posicao2)
         textoDistancia = (f"({distancia})")
-        fonte = pygame.font.Font(None, 20)
-        textoRenderizado = fonte.render(textoDistancia, True, (0, 255, 0))
+        textoRenderizado = fonte.render(textoDistancia, True, vermelho)
         tela.blit(textoRenderizado, (meioXdaLinha - textoRenderizado.get_width() // 2, meioYdaLinha - textoRenderizado.get_height() // 2))
-
+    
+    #faz as marcações
     for marcacao in marcacoes:
-        nome = marcacao["nome"]
-        posicao = marcacao["posicao"]
-        pygame.draw.circle(tela, (255, 255, 255), posicao, 10) 
+        nome = marcacao['nome']
+        posicao = marcacao['posicao']
+        pygame.draw.circle(tela, amarelo, posicao, 5) 
         texto = (f"{nome} {posicao}")
-        fonte = pygame.font.Font(None, 20)
-        textoRenderizado = fonte.render(texto, True, (255, 255, 255)) 
+        textoRenderizado = fonte.render(texto, True, branco) 
         tela.blit(textoRenderizado, (posicao[0] + 15, posicao[1] - 10))
-
+    
     pygame.display.update()
+    pygame.time.Clock().tick(fps)
 
 pygame.quit()
